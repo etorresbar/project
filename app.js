dd77da2f4e3341e3e4d5d659e30ad9117f2233fd
Eduardo Torres <etorresbar@gmail.com>
2020-10-02 11:20:38 -0700

    Adding index and app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+class Contacto{
+
+    constructor(nombre, direccion, telefono, email){
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+}
+
+class UI {
+
+    static mostrarContacto(){
+
+    }
+
+    static agregarContactoLista(){
+
+    }
+
+    static eliminarContacto(){
+
+    }
+
+    static mostrarAlerta(){
+
+    }
+
+    static limpiarCampos(){
+
+    }
+
+}
+
+class Datos{
+    
+    static traerContactos(){
+
+    }
+
+    static agregarContacto(){
+
+    }
+
+    static removerContact(){
+
+    }
+}
+
